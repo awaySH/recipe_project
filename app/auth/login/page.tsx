17aa009f06c899cd43534f4336246254a4c8af6e
awaySH <gkgkjs@gmail.com>
2024-10-13 15:04:20 +0900

    merge test

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Social login buttons will be implemented here.</p>
+    </div>
+  );
+}
---
+++ lib/auth.ts
@@ -0,0 +1,2 @@
+// This file will contain authentication-related functions and configurations
+// It will be implemented by the person working on social login
